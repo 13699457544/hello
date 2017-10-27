8eec55402a1f90e9c63a484e404cf712275eead0
xuyunhua <xuyunhua@xuyunhuadeMac-mini.local>
2017-10-27 10:35:05 +0800

    git -all ad

--- hello/ViewController.m
+++ hello/ViewController.m
@@ -17,9 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UINavigationBar* bar=[[UINavigationBar alloc] initWithFrame:CGRectMake(0, 30, [UIScreen mainScreen].bounds.size.width, 50)];
+    UINavigationItem* item=[[UINavigationItem alloc] initWithTitle:@"爱奇艺小程序"];
+    
+     UIBarButtonItem* _bar_btn=[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSearch target:self action:@selector(finish)];
+    [item setRightBarButtonItem:_bar_btn];
+    [bar setItems:[NSArray arrayWithObject:item]];
+    [self.view addSubview:bar];
 }
-
-
+-(void)finish{
+    NSLog(@"hhhhhhhhhh");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
